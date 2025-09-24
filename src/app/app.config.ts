0b74cb03f4091d7cf78251aca6eaf28602a6d7d5
kamilo <kamil.kazmierczak@pm.me>
2025-09-24 18:51:53 +0200

    Working with core-ui-starter-kit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withHashLocation,
+  withInMemoryScrolling,
+  withRouterConfig,
+  withViewTransitions
+} from '@angular/router';
 
+import { DropdownModule, SidebarModule } from '@coreui/angular';
+import { IconSetService } from '@coreui/icons-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes,
+      withRouterConfig({
+        onSameUrlNavigation: 'reload'
+      }),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      }),
+      withEnabledBlockingInitialNavigation(),
+      withViewTransitions(),
+      withHashLocation()
+    ),
+    importProvidersFrom(SidebarModule, DropdownModule),
+    IconSetService,
+    provideAnimationsAsync()
   ]
 };
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { delay, filter, map, tap } from 'rxjs/operators';
+
+import { ColorModeService } from '@coreui/angular';
+import { IconSetService } from '@coreui/icons-angular';
+import { iconSubset } from './icons/icon-subset';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +15,45 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('fast-retire-frontend');
+export class App implements OnInit {
+  title = 'CoreUI Angular Admin Template';
+
+  readonly #destroyRef: DestroyRef = inject(DestroyRef);
+  readonly #activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  readonly #router = inject(Router);
+  readonly #titleService = inject(Title);
+
+  readonly #colorModeService = inject(ColorModeService);
+  readonly #iconSetService = inject(IconSetService);
+
+  constructor() {
+    this.#titleService.setTitle(this.title);
+    // iconSet singleton
+    this.#iconSetService.icons = { ...iconSubset };
+    this.#colorModeService.localStorageItemName.set('coreui-free-angular-admin-template-theme-default');
+    this.#colorModeService.eventName.set('ColorSchemeChange');
+  }
+
+  ngOnInit(): void {
+
+    this.#router.events.pipe(
+        takeUntilDestroyed(this.#destroyRef)
+      ).subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+    });
+
+    this.#activatedRoute.queryParams
+      .pipe(
+        delay(1),
+        map(params => <string>params['theme']?.match(/^[A-Za-z0-9\s]+/)?.[0]),
+        filter(theme => ['dark', 'light', 'auto'].includes(theme)),
+        tap(theme => {
+          this.#colorModeService.colorMode.set(theme);
+        }),
+        takeUntilDestroyed(this.#destroyRef)
+      )
+      .subscribe();
+  }
 }
